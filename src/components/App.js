08d284bed27543fd9c8d6993202d7628736806c1
kiran <koffeewithkiran77@gmail.com>
2019-07-24 13:58:03 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostList from "./postList";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      hello
+      <PostList />
+    </div>
+  );
+};
+
+export default App;
