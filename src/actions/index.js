c2d2111657b6c0c15ae031b99f8fe4886a869ab1
kiran <koffeewithkiran77@gmail.com>
2019-07-26 12:26:48 +0530

    fetch user memoized

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+import _ from "lodash";
 
 const postsDispatcher = async dispatch => {
   const response = await jsonPlaceHolder.get("/posts");
@@ -9,15 +10,19 @@ const postsDispatcher = async dispatch => {
   });
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+export const fetchPosts = () => {
+  return postsDispatcher;
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceHolder.get(`/users/${id}`);
   console.log(response);
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-};
-
-export const fetchPosts = () => {
-  return postsDispatcher;
-};
+});
--- src/components/userHeader.js
+++ src/components/userHeader.js
@@ -3,21 +3,22 @@ import { connect } from "react-redux";
 import { fetchUser } from "../actions";
 class UserHeader extends React.Component {
   componentDidMount() {
-    this.props.fetchUser(this.props.userId);
+    if (!this.props.user) {
+      this.props.fetchUser(this.props.userId);
+    }
   }
 
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    const { user } = this.props;
     if (!user) {
       return null;
     }
-
     return <div className="header"> {user.username} </div>;
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(
